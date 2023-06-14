719c5084c980e3e01af8787087aa0ad1920eac58
Rostislav Rusakov <134939763+rostrskv@users.noreply.github.com>
2023-06-14 19:07:39 +0300

    Add game logic

--- week-3/basic-dom/main.js
+++ week-3/basic-dom/main.js
@@ -1,24 +1,49 @@
 "use strict"
 const playingField = document.getElementById("playing-field")
+const up = document.getElementById("up")
+const left = document.getElementById("left")
+const right = document.getElementById("right")
 const down = document.getElementById("down")
+const ball = document.getElementById("ball")
 
-console.log(down.innerHTML)
-down.innerHTML = "Down"
-
-const ball = document.getElementById("ball");
-ball.style.backgroundColor = "yellow"
-
-const moveRight = function () {
-    const left = parseInt(ball.style.left) || 0
-    ball.style.left = `${left + 15}px`
+const move = function (direction, increment) {
+    const position = (parseInt(ball.style[direction]) || 0) + increment
+    ball.style[direction] = `${position}px`
 }
-const header = document.createElement("h1")
-header.innerHTML = "The Best Game Ever"
-header.style.color = "#c0392b"
-header.style.fontFamily = "Helvetica"
-document.body.appendChild(header)
 
-const subHeader = document.createElement("h2")
-subHeader.innerHTML = "Game by: ~The Creator~"
-subHeader.classList.add("sub-header")
-document.body.appendChild(subHeader)
+const moveLeft = function () {
+    move("left", -15)
+}
+const moveRight = function () {
+    return move("left", 15)
+}
+const moveUp = function () {
+    return move("top", -15)
+}
+const moveDown = function () {
+    return move("top", 15)
+}
+
+const addHandlers = function () {
+    up.onclick = moveUp
+    left.onclick = moveLeft
+    right.onclick = moveRight
+    down.onclick = moveDown
+    
+}
+
+const addHeaders = function () {
+    const header = document.createElement("h1")
+    header.innerHTML = "The Best Game Ever"
+    header.style.color = "#c0392b"
+    header.style.fontFamily = "Helvetica"
+    document.body.appendChild(header)
+
+    const subHeader = document.createElement("h2")
+    subHeader.innerHTML = "Game by: ~The Creator~"
+    subHeader.classList.add("sub-header")
+    document.body.appendChild(subHeader)
+}
+
+addHeaders()
+addHandlers()
