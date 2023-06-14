897f73c8135e9b2edf876adf534c64d2108aca71
Rostislav Rusakov <134939763+rostrskv@users.noreply.github.com>
2023-06-14 18:45:07 +0300

    Complete first part of exercise

--- week-3/basic-dom/README.md
+++ week-3/basic-dom/README.md
@@ -1,3 +1,4 @@
+Fork from https://github.com/Elevationacademy/basic-dom
 # basic-dom
 Create by us to let you play with the DOM.
 Play nice.
--- week-3/basic-dom/main.js
+++ week-3/basic-dom/main.js
@@ -1,1 +1,24 @@
-// console.log(document)
+"use strict"
+const playingField = document.getElementById("playing-field")
+const down = document.getElementById("down")
+
+console.log(down.innerHTML)
+down.innerHTML = "Down"
+
+const ball = document.getElementById("ball");
+ball.style.backgroundColor = "yellow"
+
+const moveRight = function () {
+    const left = parseInt(ball.style.left) || 0
+    ball.style.left = `${left + 15}px`
+}
+const header = document.createElement("h1")
+header.innerHTML = "The Best Game Ever"
+header.style.color = "#c0392b"
+header.style.fontFamily = "Helvetica"
+document.body.appendChild(header)
+
+const subHeader = document.createElement("h2")
+subHeader.innerHTML = "Game by: ~The Creator~"
+subHeader.classList.add("sub-header")
+document.body.appendChild(subHeader)
